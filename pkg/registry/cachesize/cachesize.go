fae823c903ae201daf35eb180bd15bce08773875
Salvatore Dario Minonne <salvatore-dario.minonne@amadeus.com>
2016-04-20 15:27:45 +0200

    Workflow: client

--- pkg/registry/cachesize/cachesize.go
+++ pkg/registry/cachesize/cachesize.go
@@ -47,6 +47,7 @@ const (
 	Secrets                  Resource = "secrets"
 	ServiceAccounts          Resource = "serviceaccounts"
 	Services                 Resource = "services"
+	Workflows                Resource = "workflows"
 )
 
 var watchCacheSizes map[Resource]int
@@ -72,6 +73,7 @@ func init() {
 	watchCacheSizes[Secrets] = 100
 	watchCacheSizes[ServiceAccounts] = 100
 	watchCacheSizes[Services] = 100
+	watchCacheSizes[Workflows] = 100
 }
 
 func SetWatchCacheSizes(cacheSizes []string) {
